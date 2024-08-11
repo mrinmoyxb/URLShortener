ad84f4b52d70bfa67f9e0020e811567cdfe478c3
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-08-11 06:28:37 +0530

    login user controller added

--- Controller/userController.js
+++ Controller/userController.js
@@ -2,15 +2,41 @@ const express = require("express")
 const User = require("../Model/userModel")
 
 async function handlerSignUpUser(req, res){
-    const body = req.body
-    if(!body || !body.name || !body.email || !body.password){
-        return res.status(401).json({"msg":"Enter valid details"})
+    try{
+        if(!req.body || !req.body.name || !req.body.email || !req.body.password){
+            return res.status(401).json({"msg":"Enter valid credentials"})
+        }
+        const {nameOfUser, emailOfUser, passwordOfUser} = req.body;
+        await User.create({name: nameOfUser,email: emailOfUser,password: passwordOfUser})
+        return res.status(200).json({"msg": "successfully signed up"})
     }
-    const {name, email, password} = req.body;
-    await User.create({name: name,email: email,password: password})
-    return res.status(200).json({"msg": "successfully signed up"})
+    catch(error){
+        return res.status(500).json({"error": error})
+    }
+}
+
+async function handlerLoginUser(req, res){
+    try{
+        if(!req.body || !req.body.email || !req.body.password){
+            return res.status(400).json({"msg": "Enter valid credentials"})
+        }
+        const {email, password} = req.body
+        const user = await User.findOne({email, password})
+        if(!user){
+            console.log("user not available", user)
+            return res.status(400).json({"msg": "Enter valid email and password"})
+        }
+        else{
+            console.log("user is available", user)
+            return res.status(200).json({"msg": "Welcome"})
+        }
+    }
+    catch(error){
+        return res.status(500).json({"error": error})
+    }
 }
 
 module.exports = {
-    handlerSignUpUser
+    handlerSignUpUser,
+    handlerLoginUser
 }
--- Routers/userRouter.js
+++ Routers/userRouter.js
@@ -1,8 +1,9 @@
 const express = require("express")
-const {handlerSignUpUser} = require("../Controller/userController")
+const {handlerSignUpUser, handlerLoginUser} = require("../Controller/userController")
 
 const router = express.Router()
 
 router.post("/signUp", handlerSignUpUser)
+router.post("/login", handlerLoginUser)
 
 module.exports = router
